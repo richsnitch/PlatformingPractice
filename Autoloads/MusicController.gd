992b1386349ad69f01f890a231893e8e5a2d208b
Hayden <hayden12.hr@gmail.com>
2024-06-21 23:44:37 -0500

    level 3, level 3 warp, music, other random stuff

--- Autoloads/MusicController.gd
+++ Autoloads/MusicController.gd
@@ -4,6 +4,7 @@ var music
 var menu_music = load("res://Music/howsthisforamainmenu.mp3")
 var level_music = load("res://Music/jungletune.mp3")
 var settings_music = load("res://Music/Uwa_So_Rich.mp3")
+var level_3_music = load("res://Music/didyoufindeverythingokay.mp3")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,3 +24,7 @@ func play_settings_music():
 	$Music.stream = settings_music
 	$Music.play()
 	music = settings_music
+	
+func play_level3_music():
+	$Music.stream = level_3_music
+	$Music.play()
